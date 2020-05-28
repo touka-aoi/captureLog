0cbb4193bd40913db7821a09490c1b1a244225bc
touka_aoi <toukaseisei@gmail.com>
2020-05-29 00:36:39 +0900

    add queue and moveTo

--- windowcapture.py
+++ windowcapture.py
@@ -1,8 +1,15 @@
+#https://teratail.com/questions/147319
+#https://ocome85.work/2019/11/04/37/#toc11 神
+
+
 #参考 https://ocome85.work/2019/11/04/37/
 #アクティブウィンドウ名
 #アクティブウィンドいち
 #アクティブウィンドウサイズ
 #キャプチャーでおk
+
+
+#一つ前のアクティブウィンドウIDからウィンドサイズと位置を取得してキャプチャーしていいかんじにかこうてぎたらなー
 
 import tkinter
 import pyautogui
@@ -11,15 +18,24 @@ import win32con
 import threading
 import time
 import sys
+import queue
 
 global root
+global dataQ
+
+#LIFOかできた！！！
 
 def getForeWindow():
     while True:
         a = win32gui.GetWindowText(win32gui.GetForegroundWindow())
-        print(len(threading.enumerate()))
-        print(threading.enumerate())
+        dataQ.put(a)
+        # print(len(threading.enumerate()))
+        # ここ スマートじゃないポイント
+        # print(threading.enumerate())
+        # print(a)
         time.sleep(1)
+
+        # 終了措置
         if len(threading.enumerate()) == 2:
             print("are?")
             sys.exit()
@@ -28,7 +44,16 @@ def stopingpower():
     sys.exit()
 
 def getForeWindo():
-    a = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    a = dataQ.get()
+    b = win32gui.GetWindowRect(win32gui.FindWindow(None, a))
+    print(b)
+    # いけてまうかもしれん, #連打したらバグった あはははははは b[0] b[0] になってたぞ
+    if (b[0] < 0 or b[1] < 0):
+        #一回ずらす 灰天才
+        pyautogui.moveTo(-500,-500)
+        pyautogui.moveTo(b[0], b[1])
+    else:
+        pyautogui.moveTo(b[0],b[1])
     print(a)
 
 def createWindowPop():
@@ -43,10 +68,13 @@ def createWindowPop():
     root.attributes("-topmost", True)
     root.mainloop()
 
+# キューって何？
 
+# root = tkinter.Tk()
+dataQ = queue.LifoQueue()
 
-thread1 = threading.Thread(target=getForeWindow)
 thread2 = threading.Thread(target=createWindowPop)
+thread1 = threading.Thread(target=getForeWindow)
 thread1.start()
 thread2.start()
 thread1.join()
